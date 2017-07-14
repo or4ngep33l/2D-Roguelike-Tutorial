ad1d82598f8907f9d746e607ce7ad629aae5ef11
or4ngep33l <byipgit@outlook.com>
2017-07-14 02:28:05 -0700

    3. Unit Mechanics & 4. Architecture and Polish
    Completed Steps 2-6 for 3. Unit Mechanics and Adding UI & Level Transitions for 4. Architecture and Polish
    
    - note, debugging required because game starts on Day 2 and proceeding to the next level resets food

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Loader : MonoBehaviour {
 
-    public GameObject gameManager;
+public class Loader : MonoBehaviour 
+{
+	public GameObject gameManager;          //GameManager prefab to instantiate.
 
-	// Use this for initialization
-	void Awake () {
-        if (GameManager.instance == null)
-            Instantiate(gameManager);
+
+	void Awake ()
+	{
+		//Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+		if (GameManager.instance == null)
+
+			//Instantiate gameManager prefab
+			Instantiate (gameManager);
 	}
 }
